22e2aec8104f083ebed019e05e6a8d2a8d43a83a
Kalyan Rooj <kalyankumarrooj2003@gmail.com>
2026-05-17 13:07:33 +0530

    Disabled MySQL for Render deployment

--- backend/database/db_config.py
+++ backend/database/db_config.py
@@ -1,12 +1,2 @@
-import mysql.connector
-
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Rooj@156#12",
-    database="insider_threat_db"
-)
-
-cursor = db.cursor(dictionary=True)
-
-print("MySQL Connected Successfully")
+connection = None
+cursor = None
